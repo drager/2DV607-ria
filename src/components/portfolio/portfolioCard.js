e95e7b4176a93decd8904483a1f9ec99435e61a9
dav <dg222cs@student.lnu.se>
2015-12-03 21:54:47 +0100

    REFACTOR: changed layout and added to examplecard what data i need

--- src/components/portfolio/portfolioCard.js
+++ src/components/portfolio/portfolioCard.js
@@ -2,15 +2,35 @@
  * Created by dav on 2015-12-03.
  */
 import React, {Component} from 'react'
-import {Card,CardText} from 'react-mdl'
+import {Card,CardText,CardTitle} from 'react-mdl'
 
 export class PortfolioCard extends Component {
     render(){
         return (
             <div style={{width: '80%', margin: 'auto'}}>
                 <Card>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC'}}>TODO: implement backend in firebase to load portfolios</CardTitle>
                     <CardText>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor ligula nec sapien fringilla malesuada sit amet a lacus. Curabitur cursus enim urna, vitae dictum orci eleifend non. Duis pellentesque dolor massa. Sed in venenatis risus. Sed semper erat sed dignissim laoreet. Nullam eget malesuada turpis. Aenean iaculis diam ut velit vehicula, et semper nisi tincidunt. Sed eu lacus nec justo tincidunt posuere. Fusce euismod ex eget metus fringilla laoreet et nec diam. Donec mattis convallis elit sit amet tempus. Nulla consectetur, urna non sodales posuere, tellus nulla finibus arcu, non volutpat mi neque vitae est.
+                        I will need data from firebase look something like:
+                        <ul>
+                            Object start
+                            <li>
+                            imageUrl : url/to/image,
+                            </li>
+                            <li>
+                            text: EX: This project was a portfolio project to publish all my works in.,
+                            </li>
+                            <li>
+                            techniques: [Javascript,React,Redux,Firebase],
+                            </li>
+                            <li>
+                            employer:
+                                        name: LNU,
+                                        url: lnu.se
+                                        contact: david.waller@hismail.com
+                            </li>
+                            Object end
+                        </ul>
                     </CardText>
                 </Card>
             </div>
--- src/components/portfolio/portfolioCells.js
+++ src/components/portfolio/portfolioCells.js
@@ -9,9 +9,9 @@ export default class PortfolioCells extends Component {
     render(){
         return (
             <div>
-                <Cell style={{align:"stretch"}} col={6}><PortfolioCard/></Cell>
-                <Cell style={{align:"stretch"}} col={6}><PortfolioCard/></Cell>
-
+                <Cell style={{align:"stretch"}} col={3}><PortfolioCard/></Cell>
+                <Cell style={{align:"stretch"}} col={3}><PortfolioCard/></Cell>
+                <Cell style={{align:"stretch"}} col={3}><PortfolioCard/></Cell>
             </div>
         )
     }
